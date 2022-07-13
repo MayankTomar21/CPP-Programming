cdbcd7be560c19cfdb12f920f97d3a3fadd0bd9e
Mayank Tomar <81947542+MayankTomar21@users.noreply.github.com>
2022-07-13 22:47:32 +0530

    Update 2D Array.cpp

--- 2D Array.cpp
+++ 2D Array.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     
-    int s1,s2;
+    int s1,s2;  //s1 is no of rows , s2 is no of columns
     cout << "Enter size of array: ";
     cin >> s1 >> s2;
-    int arr[s1][s2];
+    int arr[s1][s2];  //total size of array is s1*s2
 
     for(int i =0; i<s1; i++) {
         for(int j =0; j<s2; j++) {
